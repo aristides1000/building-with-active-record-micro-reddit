cc1688e069519fe6e638341785c10ca225ceee5d
Omar Labana <omarlabana@outlook.com>
2021-06-10 14:51:01 +0200

    Roll back and re-migrate db

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ApplicationRecord
-  has_many :post
-  has_many :comment
+  has_many :posts
+  has_many :comments
   validates :name, presence: true, length: { in: 4..12 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..16 }
