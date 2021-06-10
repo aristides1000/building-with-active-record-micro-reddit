4af0dd303ce755c1707cebef32091df78dc8553d
Omar Labana <omarlabana@outlook.com>
2021-06-10 14:10:11 +0200

    Add Author model

---
+++ db/migrate/20210610120929_create_authors.rb
@@ -0,0 +1,11 @@
+class CreateAuthors < ActiveRecord::Migration[6.1]
+  def change
+    create_table :authors do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
