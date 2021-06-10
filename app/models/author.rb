fdc48ea4ebdc06d1c9c910b4facbd3b16e771e4b
Omar Labana <omarlabana@outlook.com>
2021-06-10 14:30:10 +0200

    Add basic validation for types and required values

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,7 @@
 class Author < ApplicationRecord
   has_many :post
   has_many :comment
+  validates :name, presence: true, length: { in: 4..12 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..16 }
 end
